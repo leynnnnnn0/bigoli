b1229e7d5f15ffd5b54f10e3765e950f5942e438
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2026-01-15 15:54:25 +0800

    now

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -906,7 +906,7 @@ export default function Welcome() {
                                 Simple, Transparent Pricing
                             </h2>
                             <p className="mx-auto max-w-2xl text-base text-black/70 sm:text-lg">
-                                Start for free.
+                                Start now for free.
                             </p>
                         </div>
 
